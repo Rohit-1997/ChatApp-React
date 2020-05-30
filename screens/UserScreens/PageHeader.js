89183b588648300d93ede09bf7448963772d0cea
BharathParvatham <bharath.parvatham@msitprogram.net>
2020-05-30 14:31:01 +0530

    Basic UI done (header, tabs color and text color)

--- screens/UserScreens/PageHeader.js
+++ screens/UserScreens/PageHeader.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/Octicons';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import Individual from './UserTabScreens/Individual';
@@ -10,26 +11,48 @@ console.log("The test printing the tab navigator", Tab);
 export default function PageHeader(props) {
     // Styling the headers
     props.navigation.setOptions({
-        title: "Msit Connect",
+        title: "MSIT Connect",
+        headerTintColor: '#FFFFFF',
+        headerStyle: {
+            backgroundColor: '#9477cb'
+        },
         headerLeft: () => (
-            <Icon 
-                name="three-bars" 
+            <Icon
+                name="three-bars"
                 style={{ paddingLeft: 15 }}
                 size={35}
-                color='#000'
+                color='#FFFFFF'
                 onPress={() => props.navigation.openDrawer()}
             />
-        )
+        ),
+
+        headerRight: () => (
+            <Icon
+                style={{ paddingRight: 15 }}
+                name='search'
+                size={25}
+                color='#FFFFFF'
+                onPress={() => props.navigation.openDrawer()}
+            />
+        ),
     })
 
     return (
         <Tab.Navigator
             tabBarOptions={{
-                activeTintColor: '#000',
-                labelStyle: {fontSize: 17}
+                activeTintColor: 'white',
+                labelStyle: { fontSize: 17 },
+                style: styles.tabs
             }}>
-            <Tab.Screen name="Individual" component={Individual}/>
+            <Tab.Screen name="Individual" component={Individual} />
             <Tab.Screen name="Groups" component={Groups} />
         </Tab.Navigator>
     )
 }
+
+const styles = StyleSheet.create({
+
+    tabs: {
+        backgroundColor: '#9477cb'
+    },
+});
