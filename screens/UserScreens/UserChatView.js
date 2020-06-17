31c20481da490a08f5d0e5293c7dc9e380986b03
BharathParvatham <bharath.parvatham@msitprogram.net>
2020-06-17 21:24:50 +0530

    Added Static Badges to Primary, Others tab

--- screens/UserScreens/UserChatView.js
+++ screens/UserScreens/UserChatView.js
@@ -1,12 +1,50 @@
 import * as React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { Thumbnail } from 'native-base';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import Primary from './UserChatDisplayScreens/Primary';
 import Others from './UserChatDisplayScreens/Others';
 
 
 const Tab = createMaterialTopTabNavigator();
+
+function IconWithBadge({ name, badgeCount, color, size }) {
+    return (
+        <View style={{ width: 70, height: 25, margin: 5 }}>
+            {/* <Ionicons name={name} size={size} color="white" /> */}
+            {badgeCount > 0 && (
+                <View
+                    style={{
+                        // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
+                        position: 'absolute',
+                        right: -15,
+                        top: 5,
+                        backgroundColor: 'white',
+                        borderRadius: 10,
+                        width: 20,
+                        height: 20,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Text style={{ color: '#9477cb', fontSize: 12, fontWeight: 'bold' }}>
+                        {badgeCount}
+                    </Text>
+                </View>
+            )}
+        </View>
+    );
+}
+
+function PrimaryIconWithBadge(props) {
+    // You should pass down the badgeCount in some other ways like React Context API, Redux, MobX or event emitters.
+    return <IconWithBadge {...props} badgeCount={3} />;
+}
+
+function OthersIconWithBadge(props) {
+    // You should pass down the badgeCount in some other ways like React Context API, Redux, MobX or event emitters.
+    return <IconWithBadge {...props} badgeCount={1} />;
+}
 
 
 export default function UserChatView(props) {
@@ -32,6 +70,28 @@ export default function UserChatView(props) {
     })
     return (
         <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ size }) => {
+                    if (route.name === 'Primary') {
+                        return (
+                            <PrimaryIconWithBadge
+                                name='ios-information-circle'
+                                size={size}
+                                color="white"
+                            />
+                        );
+                    } else if (route.name === 'Others') {
+                        return (
+                            <OthersIconWithBadge
+                                name='ios-list-box'
+                                size={size}
+                                color="white"
+                            />
+                        );
+                    }
+                },
+            })}
+
             tabBarOptions={{
                 activeTintColor: "white",
                 tabTextColor: "white",
@@ -43,6 +103,7 @@ export default function UserChatView(props) {
                     backgroundColor: 'white',
                     borderRadius: 10
                 },
+                showIcon: true
             }}>
             <Tab.Screen name="Primary">
                 {() => <Primary senderName={parameters.senderName} currentUser={parameters.currentUser} senderEmail={parameters.senderEmail} />}
