417c3936eda1a20f348e68d52fee510e7193889d
BharathParvatham <bharath.parvatham@msitprogram.net>
2020-06-17 21:31:04 +0530

    Added basic Badges to Groups chat

--- screens/UserScreens/GroupChatView.js
+++ screens/UserScreens/GroupChatView.js
@@ -10,6 +10,48 @@ import { BackHandler } from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
+function IconWithBadge({ name, badgeCount, color, size }) {
+    return (
+        <View style={{ width: 70, height: 25, margin: 5 }}>
+            {/* <Ionicons name={name} size={size} color="white" /> */}
+            {badgeCount > 0 && (
+                <View
+                    style={{
+                        // On React Native < 0.57 overflow outside of parent will not work on Android, see https://git.io/fhLJ8
+                        position: 'absolute',
+                        right: 0,
+                        top: 5,
+                        backgroundColor: 'white',
+                        borderRadius: 10,
+                        width: 20,
+                        height: 20,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Text style={{ color: '#9477cb', fontSize: 12, fontWeight: 'bold' }}>
+                        {badgeCount}
+                    </Text>
+                </View>
+            )}
+        </View>
+    );
+}
+
+function PrimaryIconWithBadge(props) {
+    // You should pass down the badgeCount in some other ways like React Context API, Redux, MobX or event emitters.
+    return <IconWithBadge {...props} badgeCount={3} />;
+}
+
+function OthersIconWithBadge(props) {
+    // You should pass down the badgeCount in some other ways like React Context API, Redux, MobX or event emitters.
+    return <IconWithBadge {...props} badgeCount={1} />;
+}
+
+function ActivitiesIconWithBadge(props) {
+    // You should pass down the badgeCount in some other ways like React Context API, Redux, MobX or event emitters.
+    return <IconWithBadge {...props} badgeCount={1} />;
+}
 
 export default function GroupChatView(props) {
     const parameters = props.route.params;             // To store a reference to the parameters passed
@@ -52,6 +94,36 @@ export default function GroupChatView(props) {
 
     return (
         <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ size }) => {
+                    if (route.name === 'Primary') {
+                        return (
+                            <PrimaryIconWithBadge
+                                name='ios-information-circle'
+                                size={size}
+                                color="white"
+                            />
+                        );
+                    } else if (route.name === 'Others') {
+                        return (
+                            <OthersIconWithBadge
+                                name='ios-list-box'
+                                size={size}
+                                color="white"
+                            />
+                        );
+                    } else if (route.name === 'Activities') {
+                        return (
+                            <OthersIconWithBadge
+                                name='ios-list-box'
+                                size={size}
+                                color="white"
+                            />
+                        );
+                    }
+                },
+            })}
+
             tabBarOptions={{
                 activeTintColor: "white",
                 tabTextColor: "white",
@@ -63,6 +135,7 @@ export default function GroupChatView(props) {
                     backgroundColor: 'white',
                     borderRadius: 10
                 },
+                showIcon: true
             }}>
             <Tab.Screen name="Primary">
                 {() => <Primary groupName={parameters.GroupName} docKey={parameters.docKey} />}
